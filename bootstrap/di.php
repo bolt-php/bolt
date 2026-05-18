5e776d0f51b5cb586934b9376cddb0aa7a5fe6d2
Faheem Anis <faheem@cartzlink.com>
2026-05-18 18:38:37 +0500

    Remove duplicate fallback handler for dependency injection

--- bootstrap/di.php
+++ bootstrap/di.php
@@ -1,7 +1,10 @@
 <?php
 
+/**
+ * @var framework\web\WebApplication $app
+ */ 
+
 use app\components\DependencyContainer;
-use framework\db\ActiveModel;
 
 $app->registerComponent('di', new DependencyContainer());
 
@@ -10,14 +13,4 @@ $app->di->scoped(\framework\web\request\Request::class, function () {
 });
 $app->di->scoped(\framework\web\request\Response::class, function () {
     return new \framework\web\request\Response();
-});
-
-$app->di->setFallback(function ($name, $type, $params) {
-    if ($params[$name]) {
-        if (is_subclass_of($type, ActiveModel::class)) {
-            return $type::find($params[$name]);
-        }
-    }
-
-    return null;
 });
