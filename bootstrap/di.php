6b956076d8540ab9b4278a3707d4802f35fc848d
Faheem Anis <faheem@cartzlink.com>
2026-05-20 18:59:29 +0500

    Used singleton request to facilitate scope sharing

--- bootstrap/di.php
+++ bootstrap/di.php
@@ -8,9 +8,9 @@ use app\components\DependencyContainer;
 
 $app->registerComponent('di', new DependencyContainer());
 
-$app->di->scoped(\framework\web\request\Request::class, function () {
+$app->di->singleton(\framework\web\request\Request::class, function () {
     return new \framework\web\request\Request();
 });
-$app->di->scoped(\framework\web\request\Response::class, function () {
+$app->di->singleton(\framework\web\request\Response::class, function () {
     return new \framework\web\request\Response();
 });
